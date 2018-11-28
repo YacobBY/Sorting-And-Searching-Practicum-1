2bc2826b253a6eeb573463ba4d15dfa29706338d
Unknown <yacob101@gmail.com>
2018-11-28 19:32:51 +0100

    begin code lenen

--- src/main/java/nl/hva/ict/ss/AdvancedSorts.java
+++ src/main/java/nl/hva/ict/ss/AdvancedSorts.java
@@ -20,10 +20,30 @@ public class AdvancedSorts {
      * @param <E>
      * @return
      */
-    public static <E extends Comparable<E>> E[] quickSort(E[] unsorted) {
+    public static <E extends Comparable<E>> E[] quickSort(E[] unsorted  )
+    {
+
         return unsorted;
+
     }
 
 
+
+//
+//    private static int partition(Comparable[] a, int lo, int hi)
+//    {
+//        int i = lo, j = hi+1;
+//        while (true)
+//        {
+//            while (less(a[++i], a[lo]))
+//                if (i == hi) break;
+//            while (less(a[lo], a[--j]))
+//                if (j == lo) break;
+//            if (i >= j) break;
+//            exch(a, i, j);
+//        }
+//        exch(a, lo, j);
+//        return j;
+//    }
 
 }
