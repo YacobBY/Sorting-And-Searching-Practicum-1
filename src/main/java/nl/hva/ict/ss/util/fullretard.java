0ff8350925a6f4bb2904ea4013351dd8cc585358
Unknown <yacob101@gmail.com>
2018-11-28 20:12:16 +0100

    minder fullretard

--- src/main/java/nl/hva/ict/ss/util/fullretard.java
+++ src/main/java/nl/hva/ict/ss/util/fullretard.java
@@ -2,28 +2,23 @@ package nl.hva.ict.ss.util;
 
 public class fullretard {
 
-    private int array[];
-    private int length;
 
-    public static void main(String a[]){
+    public static void main(String a[]) {
 
         fullretard sorter = new fullretard();
-        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        int[] input = {24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12};
         sorter.sort(input);
-        for(int i:input){
+        for (int i : input) {
             System.out.print(i + " ");
 
         }
     }
 
     public void sort(int[] inputArr) {
-
         if (inputArr == null || inputArr.length == 0) {
             return;
         }
-        this.array = inputArr;
-        length = inputArr.length;
-        quickSort(array, 0, length - 1);
+        quickSort(inputArr, 0, inputArr.length - 1);
     }
 
     private void quickSort(int[] inputArray, int lowerIndex, int higherIndex) {
@@ -31,7 +26,7 @@ public class fullretard {
         int i = lowerIndex;
         int j = higherIndex;
         // calculate pivot number, I am taking pivot as middle index number
-        int pivot = inputArray[lowerIndex+(higherIndex-lowerIndex)/2];
+        int pivot = inputArray[lowerIndex + (higherIndex - lowerIndex) / 2];
         // Divide into two arrays
         while (i <= j) {
             /**
@@ -47,7 +42,7 @@ public class fullretard {
                 j--;
             }
             if (i <= j) {
-                inputArray =  exchangeNumbers(inputArray, i, j);
+                inputArray = exchangeNumbers(inputArray, i, j);
                 //move index to next position on both sides
                 i++;
                 j--;
@@ -55,16 +50,16 @@ public class fullretard {
         }
         // call quickSort() method recursively
         if (lowerIndex < j)
-            quickSort(inputArray,lowerIndex, j);
+            quickSort(inputArray, lowerIndex, j);
         if (i < higherIndex)
-            quickSort(inputArray,i, higherIndex);
+            quickSort(inputArray, i, higherIndex);
     }
 
-    private int[] exchangeNumbers(int[] array, int lowerIndex, int higherIndex) {
-        int temp = array[lowerIndex];
-        array[lowerIndex] = array[higherIndex];
-        array[higherIndex] = temp;
-        return array;
+    private int[] exchangeNumbers(int[] inputArray, int lowerIndex, int higherIndex) {
+        int temp = inputArray[lowerIndex];
+        inputArray[lowerIndex] = inputArray[higherIndex];
+        inputArray[higherIndex] = temp;
+        return inputArray;
     }
 
 
